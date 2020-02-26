a9a7690388080a0b06193e5956615d885eedaa03
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-26 21:00:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
     else
       begin
         raise TriangleError
-      rescue TriangleError => error
+        rescue TriangleError => error
           puts error.message
       end
     end
